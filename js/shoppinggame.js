e797ce7fea668614a3ecdee33f15a81905415e6e
Ivan Luis Leiva Garcia <ileivag@proveedor.intercorp.com.pe>
2022-04-26 23:21:14 -0500

    module 2

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -262,14 +262,16 @@ const findPointsToBill = (roundedTotal) => {
 
 // Complete this function
 const findPointsForExpDate = (prod) => {
-    return prod.expiryDate < 30 ? 10 : 0;
+    return prod.daysToExpire < 30 ? 10 : 0;
 };
 
 
 const calculatePoints = (prod, tBill) => {
     let pointsToBill = findPointsToBill(Math.round(tBill));
     let pointsForExpDate = findPointsForExpDate(prod);
-    player.score += pointsToBill + pointsForExpDate;
+
+    player.score = player.score + pointsToBill + pointsForExpDate;
+    
     if (prod instanceof MagicProduct) {
         if (prod.isBonus) {
             player.addPoints(prod.points);
